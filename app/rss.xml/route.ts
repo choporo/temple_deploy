9ff1822242efe026104e09f4fca3d55e4d48e964
“choporo” <chopporo@gmail.com>
2024-12-19 17:17:37 +0900

    rewrite_rss

--- app/rss.xml/route.ts
+++ app/rss.xml/route.ts
@@ -1,14 +1,28 @@
+import { link } from "fs";
+import { templePosts } from "@/utils/posts";
 import RSS from "rss";
 
 export async function GET() {
+  const posts = templePosts;
+
   const feed = new RSS({
-    title: "Your Website",
-    description: "A cool website that everyone should check out!",
+    title: "절로 가",
+    description: "잠시 쉬어가기 좋은 우리나라 전통사찰 안내",
     site_url: "http://templeroad.kr",
     feed_url: `http://templeroad.kr/feed.xml`,
     copyright: `${new Date().getFullYear()} http://templeroad.kr`,
     language: "ko",
     pubDate: new Date(),
+  });
+
+  posts.map((post) => {
+    return {
+      title: post.title,
+      url: post.link,
+      date: post.pubDate,
+      description: post.description,
+      guid: post.link,
+    };
   });
 
   return new Response(feed.xml({ indent: true }), {
---
+++ utils/posts.ts
@@ -0,0 +1,86 @@
+export const templePosts = [
+  {
+    title: "서울의 전통사찰",
+    description: "서울에 위치한 전통사찰 목록입니다.",
+    link: "https://templeroad.kr/?category=%EC%84%9C%EC%9A%B8",
+    pubDate: new Date().toUTCString(),
+  },
+  {
+    title: "부산의 전통사찰",
+    description: "부산에 위치한 전통사찰 목록입니다.",
+    link: "https://templeroad.kr/?category=%EB%B6%80%EC%82%B0",
+    pubDate: new Date().toUTCString(),
+  },
+  {
+    title: "대구의 전통사찰",
+    description: "대구에 위치한 전통사찰 목록입니다.",
+    link: "https://templeroad.kr/?category=%EB%8C%80%EA%B5%AC",
+    pubDate: new Date().toUTCString(),
+  },
+  {
+    title: "광주/전남의 전통사찰",
+    description: "광주/전남에 위치한 전통사찰 목록입니다.",
+    link: "https://templeroad.kr/?category=%EA%B4%91%EC%A3%BC/%EC%A0%84%EB%82%A8",
+    pubDate: new Date().toUTCString(),
+  },
+  {
+    title: "인천의 전통사찰",
+    description: "인천에 위치한 전통사찰 목록입니다.",
+    link: "https://templeroad.kr/?category=%EC%9D%B8%EC%B2%9C",
+    pubDate: new Date().toUTCString(),
+  },
+  {
+    title: "대전/세종의 전통사찰",
+    description: "대전/세종에 위치한 전통사찰 목록입니다.",
+    link: "https://templeroad.kr/?category=%EB%8C%80%EC%A0%84/%EC%84%B8%EC%A2%85",
+    pubDate: new Date().toUTCString(),
+  },
+  {
+    title: "울산의 전통사찰",
+    description: "울산에 위치한 전통사찰 목록입니다.",
+    link: "https://templeroad.kr/?category=%EC%9A%B8%EC%82%B0",
+    pubDate: new Date().toUTCString(),
+  },
+  {
+    title: "경기도 전통사찰",
+    description: "경기도에 위치한 전통사찰 목록입니다.",
+    link: "https://templeroad.kr/?category=%EA%B2%BD%EA%B8%B0",
+    pubDate: new Date().toUTCString(),
+  },
+  {
+    title: "강원도 전통사찰",
+    description: "강원도에 위치한 전통사찰 목록입니다.",
+    link: "https://templeroad.kr/?category=%EA%B0%95%EC%9B%90",
+    pubDate: new Date().toUTCString(),
+  },
+  {
+    title: "충청남도 전통사찰",
+    description: "충청남도에 위치한 전통사찰 목록입니다.",
+    link: "https://templeroad.kr/?category=%EC%B6%A9%EB%82%A8",
+    pubDate: new Date().toUTCString(),
+  },
+  {
+    title: "충청북도 전통사찰",
+    description: "충청북도에 위치한 전통사찰 목록입니다.",
+    link: "https://templeroad.kr/?category=%EC%B6%A9%EB%B6%81",
+    pubDate: new Date().toUTCString(),
+  },
+  {
+    title: "전라북도 전통사찰",
+    description: "전라북도에 위치한 전통사찰 목록입니다.",
+    link: "https://templeroad.kr/?category=%EC%A0%84%EB%B6%81",
+    pubDate: new Date().toUTCString(),
+  },
+  {
+    title: "경상남도 전통사찰",
+    description: "경상남도에 위치한 전통사찰 목록입니다.",
+    link: "https://templeroad.kr/?category=%EA%B2%BD%EB%82%A8",
+    pubDate: new Date().toUTCString(),
+  },
+  {
+    title: "경상북도 전통사찰",
+    description: "경상북도에 위치한 전통사찰 목록입니다.",
+    link: "https://templeroad.kr/?category=%EA%B2%BD%EB%B6%81",
+    pubDate: new Date().toUTCString(),
+  },
+];
