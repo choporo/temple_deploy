dba566678ae1edfe4cee96f0729d2d13a98ad46c
choporo <chopporo@gmail.com>
2025-01-14 15:41:56 +0900

    renewal3

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { Suspense } from "react";
 import Logo from "./Logo";
 import NavSearch from "./NavSearch";
 import SubLogo from "./SubLogo";
 import TempleInfo from "./TempleInfo";
+import LoadingCards from "../card/LoadingCards";
 
 function Navbar() {
   return (
     <nav className="border-b">
       <div className="container flex flex-row justify-between sm:items-center flex-wrap gap-4 py-5">
         <Logo />
-        <NavSearch />
+        <Suspense fallback={<LoadingCards />}>
+          <NavSearch />
+        </Suspense>
         <div className="flex gap-4 items-center ">
           <div className="hidden sm:flex">
             <SubLogo />
